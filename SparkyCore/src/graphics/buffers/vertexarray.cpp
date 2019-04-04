0d8c337c933da845bf9bc95bdb579f5c4a991164
Tate Hertel <tate.a.hertel@gmail.com>
2019-04-04 15:00:10 -0400

    First Commit

---
+++ SparkyCore/src/graphics/buffers/vertexarray.cpp
@@ -0,0 +1,29 @@
+#include "vertexarray.h"
+
+namespace sparky { namespace graphics {
+
+	VertexArray::VertexArray()
+	{
+
+	}
+	VertexArray::~VertexArray()
+	{
+
+	}
+
+	void VertexArray::addBuffer(Buffer* buffer, GLuint index)
+	{
+
+	}
+
+	void VertexArray::bind() const
+	{
+
+	}
+
+	void VertexArray::unbind() const
+	{
+
+	}
+
+} }
